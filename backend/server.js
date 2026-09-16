4d7c9319e96ff812311af3435eb9053d868a3982
Sameer Shaik <sameer021000@gmail.com>
2026-09-16 14:44:08 +0530

    Initial commit - MERN project setup

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+    res.json({
+        message: "CRUD Website Backend is running"
+    });
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
