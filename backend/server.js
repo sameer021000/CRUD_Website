bc23287fabd4cc1ace8d6dc8cfa9fce8de6145d1
Sameer Shaik <sameer021000@gmail.com>
2026-09-17 16:36:45 +0530

    Few changes to backend before deploying to render - 2

--- backend/server.js
+++ backend/server.js
@@ -30,5 +30,5 @@ app.use(errorHandler);
 
 // Start server
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on ${PORT}`);
 });
